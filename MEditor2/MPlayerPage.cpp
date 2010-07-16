e8c896a3654e027b01c07bacf8cacbda2176004c
william0wang <william0wang@gmail.com>
2010-07-16 10:10:54 +0800

    Fixed cache size option.

--- MEditor2/MPlayerPage.cpp
+++ MEditor2/MPlayerPage.cpp
@@ -1144,63 +1144,55 @@ void CMPlayerPage::SaveConfig()
 	default:
 		m_cfg->RemoveValue(_T("ontop"));
 	}
-	
+
+	m_cfg->RemoveValue(_T("auto-cache"));
+	m_cfg->RemoveValue(_T("cache-min"));
 	int vcache = m_cache.GetCurSel();
 	switch (vcache)
 	{
 	case 1:
 		m_cfg->SetValue(_T("cache") ,_T("512") );
-		m_cfg->RemoveValue(_T("cache-min"));
 		break;
 	case 2:
 		m_cfg->SetValue(_T("cache") ,_T("1024") );
-		m_cfg->RemoveValue(_T("cache-min"));
 		break;
 	case 3:
 		m_cfg->SetValue(_T("cache") ,_T("2048") );
-		m_cfg->RemoveValue(_T("cache-min"));
 		break;
 	case 4:
 		m_cfg->SetValue(_T("cache") ,_T("4096") );
-		m_cfg->RemoveValue(_T("cache-min"));
 		break;
 	case 5:
 		m_cfg->SetValue(_T("cache") ,_T("8192") );
-		m_cfg->RemoveValue(_T("cache-min"));
 		break;
 	case 6:
 		m_cfg->SetValue(_T("cache") ,_T("16384") );
-		m_cfg->RemoveValue(_T("cache-min"));
 		break;
 	case 7:
 		m_cfg->SetValue(_T("cache") ,_T("32768") );
-		m_cfg->RemoveValue(_T("cache-min"));
 		break;
 	case 8:
 		m_cfg->SetValue(_T("cache") ,_T("65536") );
-		m_cfg->SetValue(_T("cache-min") ,_T("12") );
 		break;
 	case 9:
 		m_cfg->SetValue(_T("cache") ,_T("98304") );
-		m_cfg->SetValue(_T("cache-min") ,_T("8") );
+		m_cfg->SetValue(_T("cache-min") ,_T("10") );
 		break;
 	case 10:
 		m_cfg->SetValue(_T("cache") ,_T("131072") );
-		m_cfg->SetValue(_T("cache-min") ,_T("5") );
+		m_cfg->SetValue(_T("cache-min") ,_T("8") );
 		break;
 	case 11:
 		m_cfg->SetValue(_T("cache") ,_T("262144") );
-		m_cfg->SetValue(_T("cache-min") ,_T("3") );
+		m_cfg->SetValue(_T("cache-min") ,_T("5") );
 		break;
 	case 12:
 		m_cfg->SetValue(_T("auto-cache") ,_T("1") );
 		m_cfg->RemoveValue(_T("cache"));
-		m_cfg->RemoveValue(_T("cache-min"));
 		break;
 	default:
-		m_cfg->RemoveValue(_T("auto-cache"));
 		m_cfg->RemoveValue(_T("cache"));
-		m_cfg->RemoveValue(_T("cache-min"));
+		break;
 	}
 	
 	int vlang = m_language.GetCurSel();
