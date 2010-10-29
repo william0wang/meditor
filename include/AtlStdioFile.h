13455b1d3748ffacaf918e89de335d5c7f58074e
william0wang <william0wang@gmail.com>
2010-10-29 12:44:50 +0800

    Support code page.

--- include/AtlStdioFile.h
+++ include/AtlStdioFile.h
@@ -1,5 +1,26 @@
 #pragma once
 #include <atlfile.h>
+#include <string>
+
+static std::wstring mutil2unicode(std::string mut_str, UINT code_page)
+{
+	std::wstring outstr;
+
+	int nWideByteLength = MultiByteToWideChar(code_page,0, mut_str.c_str(), -1, NULL,0);
+
+	if(nWideByteLength <= 0)
+		nWideByteLength = mut_str.length();
+
+	wchar_t * buff = new wchar_t[nWideByteLength];
+	memset(buff, 0, sizeof(wchar_t) * nWideByteLength);
+
+	MultiByteToWideChar(code_page, 0, mut_str.c_str(), -1, buff, nWideByteLength);
+
+	outstr = buff;
+	delete buff;
+
+	return outstr;
+}
 
 class CAtlStdioFile : public CAtlFile
 {
@@ -43,6 +64,15 @@ public:
 			}
 		}
 		return S_FALSE;
+	}
+
+	void WiteUnicodeHeader()
+	{
+		BYTE buffer_byte[2];
+		buffer_byte[0] = 0xff;
+		buffer_byte[1] = 0xfe;
+		Seek(0, FILE_BEGIN);
+		Write(buffer_byte, 2);
 	}
 
 	BOOL ReadLine(CString &str)
@@ -109,6 +139,40 @@ public:
 
 		str.TrimRight('\n');
 		str.TrimRight('\r');
+
+		return TRUE;
+	}
+
+	BOOL ReadLineC(CString &str, int codepage)
+	{
+		int sp, pos;
+		CString tmp;
+		DWORD readsize = 0, size;
+		str = _T("");
+
+		ZeroMemory(buffera, buffer_size);
+		while(Read(buffera, buffer_size, size) == S_OK) {
+			if(size <= 0)
+				break;
+			readsize += size;
+			tmp += mutil2unicode(buffera, codepage).c_str();
+			sp = tmp.Find(_T('\n'));
+			if(sp >= 0) {
+				++sp;
+				str = tmp.Left(sp);
+				pos = sp - size;
+				readsize+=pos;
+				Seek(pos, FILE_CURRENT);
+				break;
+			}
+			ZeroMemory(buffera, buffer_size);
+		}
+
+		if(readsize <= 0)
+			return FALSE;
+
+		str.TrimRight(_T('\r'));
+		str.TrimRight(_T('\n'));
 
 		return TRUE;
 	}
